e9be862526572cccc6935bfedca4374bf67ff3f4
Fpierr <fanuelp9@gmail.com>
2025-09-28 23:11:59 +0200

    add test spect label and input

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,7 +1,8 @@
 import {render, screen} from "@testing-library/react";
+import { expect, test} from "@jest/globals";
 import App from "./App.jsx";
 
-test('The title h1 is rendered : School Dashboard', () => {
+test('Whether the title h1 is rendered : School Dashboard', () => {
     render(<App/>);
     const title = screen.getByText(/School Dashboard/i);
     expect(title).toBeInTheDocument();
@@ -20,3 +21,25 @@ test('check if the logo image is rendered using the alt text', () => {
     const imgLogo = screen.getByAltText(/holberton logo/i);
     expect(imgLogo).toBeInTheDocument();
 })
+
+test('Check whether 2 input elements are rendered', () => {
+    render(<App />);
+    const inputEmail = screen.getByRole("textbox");
+    const inputPassword = screen.getByLabelText(/Password/i);
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+});
+
+test('check if renders 2 label elements with text Email and Password', () => {
+    render(<App/>);
+    const labelEmail = screen.getByText(/Email/i);
+    const labelPwd = screen.getByText(/Password/i);
+    expect(labelEmail).toBeInTheDocument();
+    expect(labelPwd).toBeInTheDocument();
+});
+
+test('check if render a button text Ok', ()=> {
+    render(<App/>);
+    const buttonText = screen.getByRole('button', {name: /Ok/i});
+    expect(buttonText).toBeInTheDocument();
+});
