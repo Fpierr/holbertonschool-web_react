a432db66ef4d21f8ed0f287e4fe17fc242fe1040
Fpierr <fanuelp9@gmail.com>
2025-09-29 18:26:28 +0200

    fix err

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,36 +1,38 @@
 import "./Notifications.css";
-import closeButton from "./assets/close-button.png";
+import closeIcon from "./assets/close-button.png";
 import { getLatestNotification } from "./utils";
 
 function Notifications() {
-    return (
-        <>
-            <div className="notifications">
-                <>
-                    <button
-                        style={{
-                            float: "right",
-                            background: 'transparent',
-                            border: "none",
-                            cursor: "pointer"
-                        }}
-                        aria-label="Close"
-                        onClick={() => console.log('Close button has been clicked')}
-                    >
-                        <img src={closeButton} alt="Close" style={{ width: "10px", height: "10px" }} />
-                    </button>
-                    <>
-                        <p>Here is the list of notifications</p>
-                        <ul>
-                            <li data-priority="default" >New course available</li>
-                            <li data-priority="urgent" >New resume available</li>
-                            <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}/>
-                        </ul>
-                    </>
-                </>
-            </div>
-        </>
-    )
+  return (
+    <div className="notifications">
+      <button
+        style={{
+          position: "absolute",
+          top: "25px",
+          right: "25px",
+          background: "transparent",
+          border: "none",
+          cursor: "pointer",
+        }}
+        aria-label="Close"
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img
+          src={closeIcon}
+          alt="close"
+          style={{ width: "10px", height: "10px" }}
+        />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
 }
 
-export default Notifications
+export default Notifications;
