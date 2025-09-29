ad393949f5f56f0d579bc1405764d65d5305e222
Fpierr <fanuelp9@gmail.com>
2025-09-29 19:50:04 +0200

    add test utils

--- react_intro/task_3/dashboard/src/utils.spec.js
+++ react_intro/task_3/dashboard/src/utils.spec.js
@@ -1,27 +1,33 @@
 import { getCurrentYear, getFooterCopy, getLatestNotification } from "./utils";
+import { expect, describe, it } from '@jest/globals';
 
-describe("Utils functions", () => {
-  it("getCurrentYear should return the current year", () => {
-    const expectValue = new Date().getFullYear();
-    const funcValue = getCurrentYear();
-    expect(funcValue).toEqual(expectValue);
+describe('Test utils.js', () => {
+  describe('getCurrentYear()', () => {
+    it('Should return current year', () => {
+      const expectValue = new Date().getFullYear();
+      const funcValue = getCurrentYear();
+      expect(funcValue).toEqual(expectValue);
+    });
   });
 
-  it("getFooterCopy should return 'Holberton School' when isIndex is true", () => {
-    const expectValue = "Holberton School";
-    const funcValue = getFooterCopy(true);
-    expect(funcValue).toEqual(expectValue);
+  describe('getFooterCopy()', () => {
+    it('Should return "Holberton School" when true', () => {
+      const expectValue = "Holberton School";
+      const funcValue = getFooterCopy(true)
+      expect(funcValue).toEqual(expectValue);
+    });
+    it('Should return "Holberton School main dashboard" when false', () => {
+      const expectValue = "Holberton School main dashboard";
+      const funcValue = getFooterCopy(false)
+      expect(funcValue).toEqual(expectValue);
+    });
   });
 
-  it("getFooterCopy should return 'Holberton School main dashboard' when isIndex is false", () => {
-    const expectValue = "Holberton School main dashboard";
-    const funcValue = getFooterCopy(false);
-    expect(funcValue).toEqual(expectValue);
-  });
-
-  it("getLatestNotification should return the correct notification string", () => {
-    const expectValue = "<strong>Urgent requirement</strong> - complete by EOD";
-    const funcValue = getLatestNotification();
-    expect(funcValue).toEqual(expectValue);
+  describe('getLatestNotification()', () => {
+    it('Should return the correct value', () => {
+      const expectValue = '<strong>Urgent requirement</strong> - complete by EOD';
+      const funcValue = getLatestNotification(false)
+      expect(funcValue).toEqual(expectValue);
+    });
   });
 });
