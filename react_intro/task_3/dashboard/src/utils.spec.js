9de9a0002b98c753eb84d5c7b68d053f7bb6b87c
Fpierr <fanuelp9@gmail.com>
2025-09-29 19:49:01 +0200

    add test utils

--- react_intro/task_3/dashboard/src/utils.spec.js
+++ react_intro/task_3/dashboard/src/utils.spec.js
@@ -2,21 +2,26 @@ import { getCurrentYear, getFooterCopy, getLatestNotification } from "./utils";
 
 describe("Utils functions", () => {
   it("getCurrentYear should return the current year", () => {
-    const year = new Date().getFullYear();
-    expect(getCurrentYear()).toBe(year);
+    const expectValue = new Date().getFullYear();
+    const funcValue = getCurrentYear();
+    expect(funcValue).toEqual(expectValue);
   });
 
   it("getFooterCopy should return 'Holberton School' when isIndex is true", () => {
-    expect(getFooterCopy(true)).toBe("Holberton School");
+    const expectValue = "Holberton School";
+    const funcValue = getFooterCopy(true);
+    expect(funcValue).toEqual(expectValue);
   });
 
   it("getFooterCopy should return 'Holberton School main dashboard' when isIndex is false", () => {
-    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+    const expectValue = "Holberton School main dashboard";
+    const funcValue = getFooterCopy(false);
+    expect(funcValue).toEqual(expectValue);
   });
 
   it("getLatestNotification should return the correct notification string", () => {
-    expect(getLatestNotification()).toBe(
-      "<strong>Urgent requirement</strong> - complete by EOD"
-    );
+    const expectValue = "<strong>Urgent requirement</strong> - complete by EOD";
+    const funcValue = getLatestNotification();
+    expect(funcValue).toEqual(expectValue);
   });
 });
