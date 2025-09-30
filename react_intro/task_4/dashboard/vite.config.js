0a93e7f58ce11ea183c8d08d33adb1a8c74c2d16
Fpierr <fanuelp9@gmail.com>
2025-09-30 14:41:30 +0200

    add

--- react_intro/task_4/dashboard/vite.config.js
+++ react_intro/task_4/dashboard/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "tree/main/react_intro/task_4/dashboard/",
+  base: './',
+  // base: "tree/main/react_intro/task_4/dashboard/",
 })
