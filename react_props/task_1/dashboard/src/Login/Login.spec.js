8fef4048b2a29444f91e0ec790f218403b6c1ed8
Fpierr <fanuelp9@gmail.com>
2025-10-02 00:15:03 +0200

    add log

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -1,31 +1,22 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { expect, test } from "@jest/globals";
 import Login from "./Login.jsx";
 
-test("Check whether 2 labels, 2 inputs, and 1 button are rendered", () => {
+test("Check if email input gets focus when clicking its label", async () => {
   render(<Login />);
-  const textInputs = screen.getAllByRole('textbox');
-  const passwordInput = screen.getByLabelText(/password/i);
-  expect([...textInputs, passwordInput]).toHaveLength(2);
-  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
-  expect(passwordInput).toBeInTheDocument();
-  expect(screen.getByRole("button", { name: /ok/i })).toBeInTheDocument();
-});
-
-test("Check if email input gets focus when clicking its label", () => {
-  render(<Login />);
-  const emailInput = screen.getByLabelText(/email/i);
   const emailLabel = screen.getByText(/email:/i);
-  fireEvent.click(emailLabel);
-  emailInput.focus();
+  const emailInput = screen.getByLabelText(/email/i);
+
+  await userEvent.click(emailLabel);
   expect(emailInput).toHaveFocus();
 });
 
-test("Check if password input gets focus when clicking its label", () => {
+test("Check if password input gets focus when clicking its label", async () => {
   render(<Login />);
-  const passwordInput = screen.getByLabelText(/password/i);
   const passwordLabel = screen.getByText(/password:/i);
-  fireEvent.click(passwordLabel);
-  passwordInput.focus();
+  const passwordInput = screen.getByLabelText(/password/i);
+
+  await userEvent.click(passwordLabel);
   expect(passwordInput).toHaveFocus();
 });
