7fe796d3d6d86dcce44ecad84f91f925e7b2c999
Fpierr <fanuelp9@gmail.com>
2025-10-01 23:28:48 +0200

    add login spec

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -4,16 +4,16 @@ import Login from "./Login.jsx";
 
 test("Check whether 2 labels, 2 inputs, and 1 button are rendered", () => {
   render(<Login />);
-  expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
-  expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
+  expect(screen.getAllByLabelText(/email|password/i)).toHaveLength(2);
+  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
   expect(screen.getByRole("button", { name: /ok/i })).toBeInTheDocument();
 });
 
 test("Check if email input gets focus when clicking its label", () => {
   render(<Login />);
-  const emailInput = screen.getByLabelText(/Email/i);
-  const emailLabel = screen.getByText(/Email:/i);
+  const emailInput = screen.getByLabelText(/email/i);
+  const emailLabel = screen.getByText(/email:/i);
   fireEvent.click(emailLabel);
-  emailInput.focus();
   expect(emailInput).toHaveFocus();
 });
