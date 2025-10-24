8cb1dae453278b9d2a69aef752e3156aa85b97fe
Fpierr <fanuelp9@gmail.com>
2025-10-24 04:17:48 +0200

    add files

--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types';
 
 CourseListRow.propTypes = {
-    isHeader: PropTypes.bool.isRequired,
-    textFirstCell: PropTypes.string.isRequired,
-    textSecondCell: PropTypes.string.isRequired,
+  isHeader: PropTypes.bool.isRequired,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string,
 };
 
 export default function CourseListRow({
-    isHeader = false,
-    textFirstCell = '',
-    textSecondCell = null
+  isHeader = false,
+  textFirstCell = '',
+  textSecondCell = null,
 }) {
-    return (
-        isHeader ? (
-            <tr>
-                <th colSpan={textSecondCell ? 1 : 2}>{textFirstCell}</th>
-                {textSecondCell ? <th>{textSecondCell}</th> : null}
-            </tr>
-        ) : (
-            <tr>
-                <td>{textFirstCell}</td>
-                <td>{textSecondCell}</td>
-            </tr>
-        )
-    )
+  const style = isHeader
+    ? { backgroundColor: 'var(--color-table-header)', opacity: 0.66 }
+    : { backgroundColor: 'var(--color-table-rows)', opacity: 0.45 };
+
+  return isHeader ? (
+    <tr style={style}>
+      <th colSpan={textSecondCell ? 1 : 2}>{textFirstCell}</th>
+      {textSecondCell ? <th>{textSecondCell}</th> : null}
+    </tr>
+  ) : (
+    <tr style={style}>
+      <td>{textFirstCell}</td>
+      <td>{textSecondCell}</td>
+    </tr>
+  );
 }
