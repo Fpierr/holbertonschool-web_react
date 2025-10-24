139c018c04e271293a5bc77d2ad623ac6abf49f3
Fpierr <fanuelp9@gmail.com>
2025-10-24 04:28:22 +0200

    add files

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -1,52 +1,68 @@
-import React from 'react';
-import CourseListRow from './CourseListRow';
 import PropTypes from 'prop-types';
-import './CourseList.css';
-import WithLogging from '../HOC/WithLogging';
+import { StyleSheet, css } from 'aphrodite';
+import CourseListRow from './CourseListRow';
 
-class CourseList extends React.Component {
-  render() {
-    const { courses = [] } = this.props;
-
-    return (
-      <div className="courses">
-        {courses.length > 0 ? (
-          <table id="CourseList">
-            <thead>
-              <CourseListRow textFirstCell="Available courses" isHeader={true} />
-              <CourseListRow
-                textFirstCell="Course name"
-                textSecondCell="Credit"
-                isHeader={true}
-              />
-            </thead>
-            <tbody>
-              {courses.map((course) => (
-                <CourseListRow
-                  key={course.id}
-                  textFirstCell={course.name}
-                  textSecondCell={course.credit}
-                />
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <table id="CourseList">
-            <thead>
-              <CourseListRow
-                isHeader={true}
-                textFirstCell="No course available yet"
-              />
-            </thead>
-          </table>
-        )}
-      </div>
-    );
-  }
-}
+const styles = StyleSheet.create({
+    courses: {
+        margin: '130px auto',
+        width: '90%',
+        height: '33vh',
+    },
+    courseTable: {
+        width: '100%',
+    },
+    table: {
+        borderCollapse: 'collapse',
+        border: '2px solid rgb(161, 161, 161)',
+    },
+    th: {
+        border: '2px solid rgb(161, 161, 161)',
+        padding: '10px',
+        backgroundColor: '#f4f4f4',
+    },
+    td: {
+        border: '2px solid rgb(161, 161, 161)',
+        padding: '10px',
+    },
+});
 
 CourseList.propTypes = {
-  courses: PropTypes.array.isRequired,
+    courses: PropTypes.array.isRequired,
 };
 
-export default WithLogging(CourseList);
+export default function CourseList({ courses = [] }) {
+    return (
+        <div className={css(styles.courses)}>
+            {courses.length > 0 ? (
+                <table id="CourseList" className={css(styles.courseTable)}>
+                    <thead>
+                        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+                        <CourseListRow
+                            textFirstCell="Course name"
+                            textSecondCell="Credit"
+                            isHeader={true}
+                        />
+                    </thead>
+                    <tbody>
+                        {courses.map((course) => (
+                            <CourseListRow
+                                key={course.id}
+                                textFirstCell={course.name}
+                                textSecondCell={course.credit}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <table id="CourseList" className={css(styles.courseTable)}>
+                    <thead>
+                        <CourseListRow
+                            isHeader={true}
+                            textFirstCell="No course available yet"
+                        />
+                    </thead>
+                </table>
+            )}
+        </div>
+    );
+}
--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,29 +1,35 @@
+
 import PropTypes from 'prop-types';
 
+const headerStyle = { backgroundColor: '#deb5b545' };
+const rowStyle = { backgroundColor: '#f5f5f5ab' };
+
 CourseListRow.propTypes = {
-  isHeader: PropTypes.bool.isRequired,
-  textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.string,
+    isHeader: PropTypes.bool.isRequired,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.string.isRequired,
 };
 
 export default function CourseListRow({
-  isHeader = false,
-  textFirstCell = '',
-  textSecondCell = null,
+    isHeader = false,
+    textFirstCell = '',
+    textSecondCell = null,
 }) {
-  const style = isHeader
-    ? { backgroundColor: 'var(--color-table-header)', opacity: 0.66 }
-    : { backgroundColor: 'var(--color-table-rows)', opacity: 0.45 };
+    const style = isHeader ? headerStyle : rowStyle;
 
-  return isHeader ? (
-    <tr style={style}>
-      <th colSpan={textSecondCell ? 1 : 2}>{textFirstCell}</th>
-      {textSecondCell ? <th>{textSecondCell}</th> : null}
-    </tr>
-  ) : (
-    <tr style={style}>
-      <td>{textFirstCell}</td>
-      <td>{textSecondCell}</td>
-    </tr>
-  );
+    return (
+        <tr style={style}>
+            {isHeader ? (
+                <>
+                    <th colSpan={textSecondCell ? 1 : 2}>{textFirstCell}</th>
+                    {textSecondCell && <th>{textSecondCell}</th>}
+                </>
+            ) : (
+                <>
+                    <td>{textFirstCell}</td>
+                    <td>{textSecondCell}</td>
+                </>
+            )}
+        </tr>
+    );
 }
