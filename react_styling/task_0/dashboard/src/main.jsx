7c9ebf26676669ee7907f08035ae0d8631fb8fb5
Fpierr <fanuelp9@gmail.com>
2025-10-25 15:37:36 +0200

    add

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './App/App.jsx';
 import './main.css';
+import App from './App/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
