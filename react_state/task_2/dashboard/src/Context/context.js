6c8950a44605bf316f40e236d8dacfae9ec8a6a8
Fpierr <fanuelp9@gmail.com>
2025-10-30 03:31:51 +0100

    add

---
+++ react_state/task_2/dashboard/src/Context/context.js
@@ -0,0 +1,14 @@
+export const user = {
+    email: "",
+    password: "",
+    isLoggedIn: false,
+};
+
+export const logOut = () => {};
+
+const newContext = React.createContext({
+    user,
+    logOut,
+});
+
+export default newContext;
