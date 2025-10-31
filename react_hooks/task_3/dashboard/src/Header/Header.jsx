eda84b6f7ab3f8ad1bf6f8c5f256f44b395d0dbb
Fpierr <fanuelp9@gmail.com>
2025-10-31 05:48:40 +0100

    add

---
+++ react_hooks/task_3/dashboard/src/Header/Header.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import holbertonLogo from "../assets/holberton-logo.jpg";
+import newContext from "../Context/context";
+
+const Header = () => {
+  const { user, logOut } = useContext(newContext);
+
+  return (
+    <header className="App-header flex items-center border-b border-gray-200 p-6">
+      <img src={holbertonLogo} className="h-20 w-20" alt="Holberton logo" />
+      <h1 className="text-[var(--main-color)] text-4xl font-bold ml-4">
+        School dashboard
+      </h1>
+
+      {/* Section logout if isLoggedIn */}
+      {user.isLoggedIn && (
+        <section id="logoutSection" className="ml-auto text-right">
+          <p>
+            Welcome {user.email} (
+            <span
+              onClick={logOut}
+              style={{ cursor: "pointer", color: "blue" }}
+            >
+              logout
+            </span>
+            )
+          </p>
+        </section>
+      )}
+    </header>
+  );
+}
+
+
+export default Header;
