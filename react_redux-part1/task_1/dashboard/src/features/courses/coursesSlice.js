c48201af5d025fb1fec1b5f13aec0091d1196327
Fpierr <fanuelp9@gmail.com>
2025-11-11 18:55:34 +0100

    add

--- react_redux-part1/task_1/dashboard/src/features/courses/coursesSlice.js
+++ react_redux-part1/task_1/dashboard/src/features/courses/coursesSlice.js
@@ -1,13 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { logout } from '../auth/authSlice';
+
+const API_BASE_URL = 'http://localhost:5173';
+
+const ENDPOINTS = {
+  courses: `${API_BASE_URL}/courses.json`,
+};
 
 const initialState = {
-    courses: [],
-}
+  courses: [],
+};
+
+export const fetchCourses = createAsyncThunk(
+  'courses/fetchCourses',
+  async () => {
+    const response = await fetch(ENDPOINTS.courses);
+    const data = await response.json();
+    return data;
+  }
+);
 
 const coursesSlice = createSlice({
-    name: "courses",
-    initialState,
-    reducers: {},
+  name: 'courses',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCourses.fulfilled, (state, action) => {
+        state.courses = action.payload;
+      })
+      .addCase(logout, (state) => {
+        state.courses = [];
+      });
+  },
 });
 
 export default coursesSlice.reducer;
