7b7f5c03a856e5137a852d4053b60b3efa163b50
Fpierr <fanuelp9@gmail.com>
2025-11-11 21:05:24 +0100

    add 3

--- react_redux-part1/task_3/dashboard/src/components/Footer/Footer.jsx
+++ react_redux-part1/task_3/dashboard/src/components/Footer/Footer.jsx
@@ -1,33 +1,25 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { getCurrentYear, getFooterCopy } from "../../utils/utils";
-import { logout } from "../../features/auth/authSlice";
 
-function Footer() {
-  const dispatch = useDispatch();
-  const { user, isLoggedIn } = useSelector((state) => state.auth);
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch(logout());
-  };
-
+function Footer({ user, logOut }) {
   return (
     <footer className="App-footer text-center text-sm border-t-4 border-[var(--main-color)] mt-10 py-4 text-gray-600">
       <p>
         Copyright {getCurrentYear()} - {getFooterCopy(true)}
       </p>
 
-      {/* Display logout if login else Contact us */}
-      {isLoggedIn ? (
+      {/* Display contact / logout based on login status */}
+      {user?.isLoggedIn ? (
         <p>
           Welcome {user.email} (
           <a
             href="#logout"
-            onClick={handleLogout}
-            style={{ color: "blue", cursor: "pointer" }}
+            onClick={(e) => {
+              e.preventDefault();
+              logOut();
+            }}
           >
-            logout
+            Logout
           </a>
           )
         </p>
