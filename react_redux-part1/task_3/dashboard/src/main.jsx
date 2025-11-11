96a04a1304e607b88fc063b19797c5a68be91af8
Fpierr <fanuelp9@gmail.com>
2025-11-11 20:35:10 +0100

    add task 3

--- react_redux-part1/task_3/dashboard/src/main.jsx
+++ react_redux-part1/task_3/dashboard/src/main.jsx
@@ -2,13 +2,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css'
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx'
 import './main.css';
+import store from './app/store';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
