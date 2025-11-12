48ad2bd280fc25c097252cefc2d78853dfad79f3
Fpierr <fanuelp9@gmail.com>
2025-11-12 14:37:00 +0100

    add

--- react_redux-part1/task_0/dashboard/src/components/Header/Header.jsx
+++ react_redux-part1/task_0/dashboard/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import holbertonLogo from "../../assets/holberton-logo.jpg";
 
 function Header({ user, logOut }) {
   return (
-    <header className="App-header flex items-center border-b border-gray-200 p-6">
+    <header className="App-header flex items-center p-6">
       <img src={holbertonLogo} className="h-20 w-20" alt="Holberton logo" />
       <h1 className="text-[var(--main-color)] text-4xl font-bold ml-4">
         School dashboard
